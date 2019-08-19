c9771bd07c66a66fcc7364ffc60bf0ca86895a0f
Joe Gerhard <jlgerhard@gmail.com>
2019-08-19 11:28:22 -0500

    add main.js and link to header partial, remove ability to input blank date on new flight input.

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+
+// ------------------ event listeners --------------------//
+
+document.querySelector('body').addEventListener('click', handleClick);
+
+// ------------------ functions --------------------------//
+
+function handleClick(evt) {
+    if(evt.target.id === 'dest-button') {
+    
+    }
+}
